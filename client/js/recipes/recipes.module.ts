85e5b7e6074260f8938be13e8d426a2bdc203000
Alexandre Clément <web@alexandreclement.com>
2017-01-28 13:52:29 -0500

    Fix dropdown + add Fraction.js

--- client/js/recipes/recipes.module.ts
+++ client/js/recipes/recipes.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { CoreModule } from '../core/core.module';
 import { RecipeService } from './recipe.service';
@@ -13,7 +14,7 @@ import { RecipeFormComponent } from './recipe-form.component';
 import { RecipesActions } from '../core/redux.actions';
 
 @NgModule({
-    imports: [ CoreModule, CommonModule, FormsModule, RouterModule ],
+    imports: [ CoreModule, CommonModule, FormsModule, NgbModule, RouterModule ],
     declarations: [ CapitalizePipe, IngredientUnitPipe, RecipeDetailComponent, RecipeFormComponent ],
     providers: [ CanDeactivateRecipeDetail, RecipesActions, RecipeService ],
 })
