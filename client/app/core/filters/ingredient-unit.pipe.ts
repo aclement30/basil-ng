0bf2621984137ba976a154a80506b048493e8cc5
Alexandre Clément <web@alexandreclement.com>
2018-04-14 15:00:16 -0400

    Fix units translation

--- client/app/core/filters/ingredient-unit.pipe.ts
+++ client/app/core/filters/ingredient-unit.pipe.ts
@@ -1,27 +1,24 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Pipe({name: 'ingredientUnit'})
 export class IngredientUnitPipe implements PipeTransform {
-    transform(value: string): string {
-        switch (value) {
-            case 'cup':
-                return 'tasse';
-            case 'box':
-                return 'boîte';
-            case 'tablet':
-                return 'tablette';
-            case 'bit':
-                return 'morceau';
-            case 'packet':
-                return 'sachet';
-            case 'tsp':
-                return 'c. à c.';
-            case 'tbsp':
-                return 'c. à s.';
-            case 'pinch':
-                return 'pincée';
-            default:
-                return value;
-        }
+
+  constructor(private translate: TranslateService) {}
+
+  transform(value: string): string {
+    switch (value) {
+      case 'cup':
+      case 'box':
+      case 'tablet':
+      case 'bit':
+      case 'packet':
+      case 'tsp':
+      case 'tbsp':
+      case 'pinch':
+        return this.translate.instant(`units.${value}`);
+      default:
+        return value;
     }
+  }
 }
