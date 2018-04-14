2a8a2a1e71cc3170a8564cfcf9278a0a2e12055a
Alexandre Clément <web@alexandreclement.com>
2018-04-14 00:17:47 -0400

    Update footer

--- client/app/core/footer.component.ts
+++ client/app/core/footer.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
     selector: 'footer',
     template: `
-        Copyright &copy; 2018 - Alexandre Clément
+      <ul class="menu">
+        <li><a (click)="switchLanguage('en')" *ngIf="language !== 'en'">English</a></li>
+        <li><a (click)="switchLanguage('fr')" *ngIf="language !== 'fr'">Français</a></li>
+      </ul>
+      <div class="copyright">Copyright &copy; 2018 - <a href="http://www.alexandreclement.com" target="_blank">Alexandre Clément</a></div>
     `,
     styleUrls: ['footer.component.scss'],
 })
 
-export class FooterComponent { }
+export class FooterComponent {
+  constructor(
+    private translate: TranslateService,
+  ) {}
+
+  get language(): string {
+    return this.translate.currentLang;
+  }
+
+  switchLanguage(language: string) {
+    this.translate.use(language);
+    localStorage.setItem('basil-language', language);
+  }
+}
