a7bcfd1a3341c15474b026e87467f5c0e4d71881
Alexandre Clément <web@alexandreclement.com>
2018-04-14 15:00:28 -0400

    Replace fraction in ingredients

--- server/services/recipe-formatter.js
+++ server/services/recipe-formatter.js
@@ -20,12 +20,28 @@ class RecipeFormatterService {
             let description = ingredient.description;
 
             if (description) {
-                description = description.trim();
-                description = description.replace(/([\n\s])+/g, " ");
-                description = description.replace(/’/, "'");
-                description = description.replace(/([0-9]),([0-9])/g, '$1.$2');
+              description = description.trim();
+              description = description.replace(/([\n\s])+/g, " ");
+              description = description.replace(/’/, "'");
+              description = description.replace(/([0-9]),([0-9])/g, '$1.$2');
+              description = description.replace(/¼/, '1/4');
+              description = description.replace(/½/, '1/2');
+              description = description.replace(/¾/, '3/4');
+              description = description.replace(/⅓/, '1/3');
+              description = description.replace(/⅔/, '2/3');
+              description = description.replace(/⅕/, '1/5');
+              description = description.replace(/⅖/, '2/5');
+              description = description.replace(/⅗/, '3/5');
+              description = description.replace(/⅘/, '4/5');
+              description = description.replace(/⅙/, '1/6');
+              description = description.replace(/⅚/, '5/6');
+              description = description.replace(/⅛/, '1/8');
+              description = description.replace(/⅜/, '3/8');
+              description = description.replace(/⅝/, '5/8');
+              description = description.replace(/⅞/, '7/8');
+              description = description.replace(/⅒/, '1/10');
 
-                ingredient.description = description;
+              ingredient.description = description;
             }
 
             return ingredient;
